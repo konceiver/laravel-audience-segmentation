0e6b83b9fa26442b9e71f4a20d671bc91523953e
Brian Faust <hello@basecode.sh>
2021-07-25 05:46:36 +0300

    maintenance

--- config/skeleton.php
+++ config/skeleton.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 /*
  * This file is part of Skeleton.
  *
- * (c) Konceiver Oy <info@konceiver.dev>
+ * (c) Konceiver <info@konceiver.dev>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
